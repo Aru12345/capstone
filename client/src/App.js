6025ce486c115ba8b191806d0a844a5c56a5f51b
Arundhati <arundhatiraicar12@gmail.com>
2022-08-17 14:26:04 +0530

    Video Implmented and styled

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import TimesSquare from './components/TimesSquare';
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+  
       <TimesSquare />
     </div>
   );
--- client/src/components/TimesSquare.js
+++ client/src/components/TimesSquare.js
@@ -1,23 +1,20 @@
-import ts from '../mediafiles/ts.mp4'
-import test from '../mediafiles/test.png'
+
 import tsquare from '../mediafiles/tsquare.mp4'
-import {Link} from 'react-router-dom'
-import { BrowserRouter } from "react-router-dom";
+import "./TimesSquare.css";
+import { Link } from 'react-router-dom';
 function TimesSquare(){
     return(
         <>
-     <div id="ts">
-        <video autoPlay muted loop id="myVideo">
-        <source src={tsquare} type="video/mp4"/> 
-        <Link className="explore" to="/home">EXPLORE DC</Link>
-      </video>
+     <div class="video-container">
+    <video autoPlay muted loop id="myVideo">
+        <source  src={tsquare} type="video/mp4"/> 
+     
+     </video>
+     <Link className="tour" to="/home">Get Started 🍎</Link>
+     </div>
+   
   
-    </div>
-    <div id="image">
-        <img src="pic.jpg" />
-    </div>
-    <img src={test}  width="500" height="600"></img>
-        </>
+     </>
     )
 }
 export default TimesSquare;
