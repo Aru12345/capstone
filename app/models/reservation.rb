c064cbe5e22f5277b769816b49204497b899b8c2
Arundhati <arundhatiraicar12@gmail.com>
2022-08-26 20:32:41 +0530

    Added validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,17 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  validates :name,:date,:time,:num,:contact,presence: true
+  validates :contact,length: {in: 4..15}
+  validate :choosing_valid_date
+
+  def choosing_valid_date
+    if date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
+
 end
 
 # attributes :id, :name, :date, :time, :num, :contact, :occasion
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  validates :r, length: { maximum: 1000,
+  too_long: "%{1000} characters is the maximum allowed" }
 end
+
+#attributes :id, :img, :r
